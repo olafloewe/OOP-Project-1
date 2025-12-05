23be646832623ff4b821c844309d32d080034256
Merito <Merito@WSB-POZNAN>
2025-12-05 12:11:29 +0100

    delegates working in GUI

--- Project 1/Project 1/GUI.cs
+++ Project 1/Project 1/GUI.cs
@@ -7,36 +7,51 @@ using System.Threading.Tasks;
 namespace Project_1 {
     internal class GUI {
 
+        private delegate void Delegate();
+
         public GUI() {  
         
         }
          
-        public static void StartMenu() {
-            SelectionPage(new string[] { "argument 1", "argument 2", "argument 3", "argument 4" });
-            Dictionary<String, Action> link = new Dictionary<String, Action>;
-            link.Add("argument 1", DoSomething());
-            
-            SelectionPage(link); 
+        public static void StartMenu(){
+            Dictionary<String, Delegate> link = new Dictionary<String, Delegate>();
+            // create delegates for each page
+            Delegate login = new Delegate(Login);
+            Delegate employeeLookUp = EmployeeLookUp;
+            Delegate scheduleLookUp = ScheduleLookUp;
 
+            // adds delegates and arguments to dictionary
+            link.Add("Login", login);
+            link.Add("Employee Lookup", employeeLookUp);
+            link.Add("Schedule Loopup", scheduleLookUp);
+
+            // asks user to select a page
+            SelectionPage(link);
         }
-         
-        private delegate void DoSomething() { 
-            
-        };
+
+        private static void Login(){
+            Console.WriteLine("Login Page");
+        }
+
+        private static void EmployeeLookUp(){
+            Console.WriteLine("Employee Lookup Page");
+        }
+
+        private static void ScheduleLookUp(){
+            Console.WriteLine("Schedule Lookup Page");
+        }
 
         // builds a selection page
-        private static void SelectionPage(string[] args) {
-
+        private static void SelectionPage(Dictionary<String, Delegate> dict) {
+            
             // print options
-            for (int i = 0; i < args.Length; i++) { 
-                Console.WriteLine($"{i+1}. {args[i]}");
-            }
+            for (int i = 0; i < dict.Count() ; i++) Console.WriteLine($"{i+1}. {dict.Keys.ToArray()[i]}");
 
             // ask for input
-            int selection = ReadInput(args.Length);
+            int selection = ReadInput(dict.Count());
 
-            // display input
-            Console.WriteLine($"\nselection made: {selection}");
+            // execute selected option
+            dict.Values.ToArray()[selection].Invoke();
         }
 
         // read and return a key input stroke from the console to be used in user input for GUI
